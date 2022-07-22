0e38a2f7fb3501befe31efd9646182a274cd6340
benjyiw <ben@wese.io>
2022-07-21 19:50:51 -0600

    feat: implement publish by name, safely escape query parameters
    add a PublishByID method to the publish service, refactor publish to
    publish by name, since that is the more likely usecase of the publish
    service.
    
    add escaping to query parameters to prevent "unexpected EOF" when
    pipelines with spaces are supplied.
    
    update tests.

--- publish.go
+++ publish.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 const publishPath = "authenticated/publish"
@@ -11,10 +12,27 @@ const publishPath = "authenticated/publish"
 // PublishService handles communication to the publish API endpoint
 type PublishService service
 
-// Publish sends data to a specified pipeline. The data input is sent as the
-// body of the request.
-func (s *PublishService) Publish(ctx context.Context, pipelineID string, data interface{}) (*http.Response, error) {
-	path := fmt.Sprintf("%s?pipelineId=%s", publishPath, pipelineID)
+// Publish sends data to a specified pipeline by it's name. The data input is
+// sent as the body of the request.
+func (s *PublishService) Publish(ctx context.Context, pipelineName string, data interface{}) (*http.Response, error) {
+	path := fmt.Sprintf("%s?name=%s", publishPath, url.QueryEscape(pipelineName))
+	req, err := s.client.NewRequestWithCustomerURL("POST", path, data)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.client.DoRequest(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// PublishByID sends data to a specified pipeline by it's ID. The data input is
+// sent as the body of the request.
+func (s *PublishService) PublishByID(ctx context.Context, pipelineID string, data interface{}) (*http.Response, error) {
+	path := fmt.Sprintf("%s?id=%s", publishPath, url.QueryEscape(pipelineID))
 	req, err := s.client.NewRequestWithCustomerURL("POST", path, data)
 	if err != nil {
 		return nil, err
