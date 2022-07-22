2462f8ee3aeda1c3a3c0147e5ed9815475f3b0d3
ben <ben@wese.io>
2022-07-22 13:19:16 -0600

    feat: implement publish by name, safely escape query parameters (#3)
    add a PublishByID method to the publish service, refactor publish to
    publish by name, since that is the more likely usecase of the publish
    service.
    
    add escaping to query parameters to prevent "unexpected EOF" when
    pipelines with spaces are supplied.
    
    update tests.

--- publish_test.go
+++ publish_test.go
@@ -9,11 +9,35 @@ import (
 )
 
 const (
-	testPublishPipelineID = "ABC123"
+	testPublishPipelineName = "testPipeline"
+	testPublishPipelineID   = "ABC123"
 )
 
 func TestPublish_Publish(t *testing.T) {
+	client, mux, teardown := setup()
+	defer teardown()
 
+	mux.HandleFunc("/authenticated/publish", func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, "POST", r.Method)
+		require.Contains(t, r.Header, "Authorization")
+	})
+
+	ctx := context.Background()
+	data := struct {
+		TestKey string
+	}{
+		TestKey: "TestValue",
+	}
+	// validate a generic publish attempt
+	_, err := client.Publish.Publish(ctx, testPublishPipelineName, data)
+	require.NoError(t, err)
+
+	// ensure that query parameters are safely escaped
+	_, err = client.Publish.Publish(ctx, "pipeline with spaces", data)
+	require.NoError(t, err)
+}
+
+func TestPublish_PublishByID(t *testing.T) {
 	client, mux, teardown := setup()
 	defer teardown()
 
